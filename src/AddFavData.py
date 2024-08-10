cbc3cd2ee874725e6684e34f7b206fd90da6b184
eezd <eezd@outlook.com>
2024-08-10 22:08:05 +0800

    Optimize AddFavData() & SQL
    1. 优化了 AddFavData() 中所有SQL语句
    2. 新增了 AddFavData() 部分注解
    3. 重写了 AddFavData().apply() 关于画廊存在新版本可用的处理策略

--- src/AddFavData.py
+++ src/AddFavData.py
@@ -18,38 +18,46 @@ class AddFavData(Config):
         super().__init__()
 
     @logger.catch()
-    # 使用 EhTagTranslation 的标签数据库对 tag_list 中的标签进行翻译
     async def translate_tags(self):
+        """
+        对 tag_list 表中的标签进行中文翻译
+        Translate the tags in the `tag_list` table into Chinese.
+        """
         logger.info(f"Downloading translation database...")
-        # 下载最新的标签翻译数据库
-        database_url = "https://github.com/EhTagTranslation/Database/releases/latest/download/db.text.json"
-        database_name = "db.text.json"
-        r = await self.fetch_data(database_url)
-        with open(database_name, 'wb') as f:
+        translate_tag_url = "https://github.com/EhTagTranslation/Database/releases/latest/download/db.text.json"
+        translate_tag_name = "db.text.json"
+        r = await self.fetch_data(translate_tag_url)
+        # download file
+        with open(translate_tag_name, 'wb') as f:
             f.write(r)
-
-        # 加载数据库中的标签数据
-        with open(database_name, 'r', encoding='utf-8') as file:
+        # read file
+        with open(translate_tag_name, 'r', encoding='utf-8') as file:
             db_data = json.load(file)
+
         namespace_data = {}
         for item in db_data["data"]:
             namespace_data[item["namespace"]] = item["data"]
         with sqlite3.connect(self.dbs_name) as co:
-            result = co.execute('SELECT tid, tag FROM tag_list').fetchall()
+            result = co.execute('''SELECT tid, tag FROM tag_list''').fetchall()
+            if len(result) == 0:
+                logger.warning("The tag_list table in the database is empty.")
+                return
             for entry in result:
                 tid = entry[0]
                 tag = entry[1]
                 try:
                     namespace, tagcontent = tag.split(":", 1)
                 except ValueError:
-                    # 使用 api 获取到的部分 tag 好像没有命名空间的结构
+                    # 忽略部分tag不存在命名空间的错误
+                    # Ignore errors related to some tags lacking a namespace.
+                    logger.warning(f"Invalid tag: {tag}")
                     continue
                 if namespace in namespace_data and tagcontent in namespace_data[namespace]:
                     translated_tag = namespace_data[namespace][tagcontent]["name"]
-                    co.execute('UPDATE tag_list SET translated_tag = ? WHERE tid = ?', (translated_tag, tid))
-                    print(f"{tag.ljust(50)} -> {translated_tag}")
+                    co.execute('''UPDATE tag_list SET translated_tag = ? WHERE tid = ?''', (translated_tag, tid))
+                    # print(f"{tag.ljust(50)} -> {translated_tag}")
                     co.commit()
-        os.remove(database_name)
+        os.remove(translate_tag_name)
 
     @logger.catch()
     async def update_category(self):
@@ -65,7 +73,7 @@ class AddFavData(Config):
 
         with sqlite3.connect(self.dbs_name) as co:
             co.executemany(
-                'INSERT OR REPLACE INTO fav_name(fav_id, fav_name) VALUES (?,?)',
+                'INSERT OR REPLACE INTO fav_name(fav_id, fav_name) VALUES (?,?)''',
                 fav_category)
 
             co.commit()
@@ -137,7 +145,6 @@ class AddFavData(Config):
                 url = f'https://{self.base_url}/favorites.php?favcat={fav_id}&f_search=&next={next_gid}'
 
             hx_res = await self.fetch_data(url)
-
             hx_res_bs = BeautifulSoup(hx_res, 'html.parser')
             search_data = self.format_fav_page_info(hx_res_bs)
             await asyncio.sleep(0.5)
@@ -153,17 +160,17 @@ class AddFavData(Config):
                     all_gid.append(_gid)
             with sqlite3.connect(self.dbs_name) as co:
                 co.executemany(
-                    "INSERT OR IGNORE INTO eh_data(gid, token) VALUES (?,?)",
+                    '''INSERT OR IGNORE INTO eh_data(gid, token) VALUES (?,?)''',
                     eh_data)
                 co.commit()
 
                 co.executemany(
-                    "INSERT OR IGNORE INTO fav_category(gid, token, fav_id, del_flag) VALUES (?,?,?,0)",
+                    '''INSERT OR IGNORE INTO fav_category(gid, token, fav_id, del_flag) VALUES (?,?,?,0)''',
                     fav_category_data)
                 co.commit()
 
                 for gid, token, fav_id in fav_category_data:
-                    co.execute('UPDATE fav_category SET fav_id = ?, del_flag = 0 WHERE gid = ?', (fav_id, gid))
+                    co.execute('''UPDATE fav_category SET fav_id = ?, del_flag = 0 WHERE gid = ?''', (fav_id, gid))
                 co.commit()
 
             # 判断切换下一个收藏夹
@@ -189,6 +196,13 @@ class AddFavData(Config):
 
     @logger.catch()
     async def add_tags_data(self, get_all=False):
+        """
+        默认get_all=False只更新标题为空或NULL的TAG
+        The default behavior `get_all=False` only updates TAGs where the title is empty or NULL.
+
+        以 `eh_data` 表为准, 更新字段数据及其tag( `gid_tid` & `tag_list` )
+        Based on the `eh_data` table, update the field data and its tags (`gid_tid` and `tag_list`).
+        """
         logger.info(f'Get EH tags...')
 
         if not get_all:
@@ -197,18 +211,12 @@ class AddFavData(Config):
                     '''
                     SELECT gid, token 
                     FROM eh_data 
-                    WHERE gid NOT IN (
-                        SELECT gid 
-                        FROM gid_tid 
-                        WHERE gid IS NOT NULL
-                    ) 
-                    OR title = "" 
-                    OR title IS NULL
+                    WHERE title = "" OR title IS NULL
                     '''
                 ).fetchall()
         else:
             with sqlite3.connect(self.dbs_name) as co:
-                gid_token = co.execute('SELECT gid,token FROM eh_data').fetchall()
+                gid_token = co.execute('''SELECT gid,token FROM eh_data''').fetchall()
 
         total = len(gid_token)
         # 每次请求 25 个数据
@@ -235,6 +243,7 @@ class AddFavData(Config):
                 for post_json in post_json_arr:
                     post_data = await self.post_eh_api(post_json)
                     format_data = []
+
                     for sub_post_data in post_data:
                         gid = sub_post_data.get('gid')
                         token = sub_post_data.get('token')
@@ -267,51 +276,63 @@ class AddFavData(Config):
                             gid
                         ))
 
+                    # 向数据库插入数据 / Insert data into the database.
                     for data in format_data:
-                        co.execute(
-                            "UPDATE eh_data SET title=?, title_jpn=?, "
-                            "category=?, thumb=?, uploader=?, posted=?, filecount=?, "
-                            "filesize=?, expunged=?, rating=?, current_gid=?, "
-                            "current_token=? WHERE gid=?",
-                            (data[0], data[1], data[2], data[3], data[4], data[5],
-                             data[6], data[7], data[8], data[9], data[11],
-                             data[12], data[13])
-                        )
-                        # 当执行 add_tags_data(True) 时，先删除对应 gid 的映射以适配 tag 的删减
-                        if get_all:
-                            co.execute('DELETE FROM gid_tid WHERE gid =?', (data[13],))
-                            co.commit()
+                        gid = data[13]
+                        # Add data to the eh_data table.
+                        co.execute('''
+                            UPDATE eh_data 
+                            SET title =?,
+                            title_jpn =?,
+                            category =?,
+                            thumb =?,
+                            uploader =?,
+                            posted =?,
+                            filecount =?,
+                            filesize =?,
+                            expunged =?,
+                            rating =?,
+                            current_gid =?,
+                            current_token =? 
+                            WHERE
+                                gid =?
+                            ''',
+                                   (data[0], data[1], data[2], data[3], data[4], data[5],
+                                    data[6], data[7], data[8], data[9], data[11],
+                                    data[12], gid)
+                                   )
 
-                        # 添加 tag 数据时我的代码总是会漏一些 gid_tid 的映射 (一般应该不会太多)
-                        # 我还没找到原因(，重复 1~2 次 add_tags_data(False) 情况应该会改善一些，代码在下面 line 306
+                        # Clear tag data
+                        co.execute('DELETE FROM gid_tid WHERE gid =?', (gid,))
+                        co.commit()
+
+                        # Add tag data
                         tags = ast.literal_eval(data[10])
                         for tag in tags:
-                            result = co.execute('SELECT tid FROM tag_list WHERE tag =?', (tag,)).fetchone()
+                            result = co.execute('''SELECT tid FROM tag_list WHERE tag =?''', (tag,)).fetchone()
                             if result:
                                 tid = result[0]
                             else:
-                                co.execute('INSERT INTO tag_list (tag) VALUES (?)', (tag,))
-                                tid = co.execute('SELECT tid FROM tag_list WHERE tag =?', (tag,)).fetchone()[0]
-                            co.execute('INSERT OR IGNORE INTO gid_tid (gid, tid) VALUES (?, ?)', (data[13], tid))
+                                co.execute('''INSERT INTO tag_list (tag) VALUES (?)''', (tag,))
+                                tid = co.execute('''SELECT tid FROM tag_list WHERE tag =?''', (tag,)).fetchone()[0]
+                            co.execute('''INSERT OR IGNORE INTO gid_tid (gid, tid) VALUES (?, ?)''', (gid, tid))
                     co.commit()
-
                     progress_bar.update(piece)
 
-            # 检查遗漏数据，一般不会太多
-            missed_tag = co.execute(
-                'SELECT gid FROM eh_data WHERE gid NOT IN (SELECT gid FROM gid_tid WHERE gid IS NOT NULL)').fetchall()
-            if len(missed_tag) >= 50:
+            # 检查遗漏数据 / Check for missing data
+            missed_tag = co.execute('''
+                SELECT
+                    gid 
+                FROM
+                    eh_data 
+                WHERE
+                    gid NOT IN ( SELECT gid FROM gid_tid WHERE gid IS NOT NULL )
+                ''').fetchall()
+            if len(missed_tag) > 0:
+                logger.warning(f"Missed data: {missed_tag}")
+                logger.warning("Retry in 3 seconds")
+                await asyncio.sleep(3)
                 await self.add_tags_data()
-
-    def delete_fav_category_del_flag(self, gid_list):
-        with sqlite3.connect(self.dbs_name) as co:
-            if gid_list:
-                placeholders = ','.join('?' for _ in gid_list)
-                # 假如删除了, 那么他就丢失了该 "已下载" 画廊的所有信息
-                # co.execute(f'DELETE FROM eh_data WHERE gid IN ({placeholders})', gid_list)
-                co.execute(f'DELETE FROM fav_category WHERE gid IN ({placeholders})', gid_list)
-                # co.execute(f'DELETE FROM gid_tid WHERE gid IN ({placeholders})', gid_list)
-                co.commit()
 
     @logger.catch()
     async def apply(self):
@@ -322,29 +343,100 @@ class AddFavData(Config):
         await self.add_tags_data()
 
         with sqlite3.connect(self.dbs_name) as co:
-            co.execute(
-                'DELETE FROM eh_data WHERE gid IN (SELECT gid FROM fav_category WHERE del_flag = 1 AND original_flag = 0 AND web_1280x_flag = 0)')
+            # 清除所有 del_flag=1 并且没有下载的画廊 original_flag = 0 AND web_1280x_flag = 0
+            # Clear all items where del_flag=1 and have not been downloaded, with original_flag=0 and web_1280x_flag=0.
+            co.execute('''
+            DELETE 
+            FROM
+                eh_data 
+            WHERE
+                gid IN ( SELECT gid FROM fav_category WHERE del_flag = 1 AND original_flag = 0 AND web_1280x_flag = 0 )
+            ''')
             co.commit()
-            co.execute(
-                'DELETE FROM gid_tid WHERE gid IN (SELECT gid FROM fav_category WHERE del_flag = 1 AND original_flag = 0 AND web_1280x_flag = 0)')
+            co.execute('''
+            DELETE 
+            FROM
+                gid_tid 
+            WHERE
+                gid IN ( SELECT gid FROM fav_category WHERE del_flag = 1 AND original_flag = 0 AND web_1280x_flag = 0 )
+            ''')
             co.commit()
-            co.execute('DELETE FROM fav_category WHERE del_flag = 1 AND original_flag = 0 AND web_1280x_flag = 0')
+            co.execute('''
+            DELETE 
+            FROM
+                fav_category 
+            WHERE
+                del_flag = 1 
+                AND original_flag = 0 
+                AND web_1280x_flag = 0
+            ''')
             co.commit()
 
-            del_flag_list = co.execute(
-                'SELECT gid,token FROM fav_category WHERE del_flag = 1 AND (original_flag != 1 OR web_1280x_flag != 1)').fetchall()
+            # 搜索 del_flag=1 并且 已下载 并且 当前字段的current_gid=其他字段的gid
+            # 就可以得出结论, 当前画廊存在更新
+            # Search for records where `del_flag=1` and `already downloaded`,
+            # and where the current field's `current_gid` matches another field's `gid`.
+            # This indicates that the current gallery has updates.
+            update_list = co.execute('''
+            SELECT
+                fc.gid,
+                fc.token,
+                eh.current_gid,
+                eh.current_token 
+            FROM
+                fav_category AS fc,
+                eh_data AS eh 
+            WHERE
+                fc.del_flag = 1 
+                AND fc.gid = eh.gid
+                AND ( fc.original_flag = 1 OR fc.web_1280x_flag = 1 )
+                AND eh.gid != eh.current_gid
+                AND eh.current_gid IN ( SELECT gid FROM eh_data )
+            ''').fetchall()
+            if len(update_list) > 0:
+                logger.warning(
+                    f"Tips: 当前判断是基于 eh_data.current_gid。如果需要准确判断, 请使用`2. Update Gallery Metadata (Update Tags)` 重新获取数据")
+                logger.warning(
+                    f"Tips: The current judgment is based on eh_data.current_gid. For accurate assessment, please use "
+                    f"`2. Update Gallery Metadata (Update Tags)` to retrieve the data again.")
+                logger.warning(f"下列画廊存在新版本可用/The current gallery has a new version available.: ")
+                for gid_token in update_list:
+                    logger.warning(
+                        f"https://exhentai.org/g/{gid_token[0]}/{gid_token[1]}>>>https://exhentai.org/g/{gid_token[2]}/{gid_token[3]}")
+                await asyncio.sleep(2)
 
+            # del_flag = 1
+            gid_values = [item[0] for item in update_list]
+            placeholders = ','.join('?' for _ in gid_values)
+            del_flag_list = co.execute(f'''
+                SELECT
+                    gid,
+                    token 
+                FROM
+                    fav_category 
+                WHERE
+                    del_flag = 1 
+                    AND ( original_flag = 1 OR web_1280x_flag = 1 )
+                    AND gid NOT IN ({placeholders})
+            ''', gid_values).fetchall()
             if len(del_flag_list) > 0:
-                logger.warning(f"del_flag = 1:")
-                logger.warning(f"只有两种可能性/ Only two possibilities exist:")
-                logger.warning(f"1. 你移除了该画廊")
-                logger.warning(f"1. You have removed this gallery.")
-                logger.warning(f"2. 此图库有更新的版本可用")
-                logger.warning(f"2. There are newer versions of this gallery available:")
+                logger.warning(f"下列画廊无法根据 eh_data.current_gid 判断是否存在新版本可用")
+                logger.warning(
+                    f"The following gallery cannot determine whether a new version is available based on "
+                    f"`eh_data.current_gid`.")
+                logger.warning(f"如果你确定要在数据库中移除下列画廊, 输入确认(y/n)")
+                logger.warning(
+                    f"If you are sure you want to remove the following gallery from the database, type ‘confirm’ (y/n).")
                 for gid_token in del_flag_list:
                     logger.warning(f"https://exhentai.org/g/{gid_token[0]}/{gid_token[1]}")
 
-                del_enter = input("Delete data where del_flag = 1? (y/n):")
+                del_enter = input("\n")
                 if del_enter.lower() == 'y':
                     gid_list = [row[0] for row in del_flag_list]
-                    self.delete_fav_category_del_flag(gid_list)
+                    if gid_list:
+                        placeholders = ','.join('?' for _ in gid_list)
+                        co.execute(f'DELETE FROM eh_data WHERE gid IN ({placeholders})', gid_list)
+                        co.execute(f'''DELETE FROM fav_category WHERE gid IN ({placeholders})''', gid_list)
+                        co.execute(f'DELETE FROM gid_tid WHERE gid IN ({placeholders})', gid_list)
+                        co.commit()
+                        logger.warning(f"DELETE https://exhentai.org/g/{gid_token[0]}/{gid_token[1]}")
